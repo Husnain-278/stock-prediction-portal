4f1dd09e5ecb2254ab441353f56de93dceed1985
husnain-278 <husnain327878@gmail.com>
2026-01-12 15:58:30 +0500

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1>hello how are ujfdj d dj i fjf and what about youf ff dfj djfkd fjff fdsfdj</h1>
+    </>
+  )
+}
+
+export default App
